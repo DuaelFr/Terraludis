2b5b2db48bb4ed8005caf587175f0d854b4406f4
Edouard Cunibil <admin@duael.fr>
2012-03-03 12:08:41 +0100

    TL : modification theme pour gerer les filtres univers sur les vues

--- sites/terraludis.org/themes/tl_ruban/js/scripts.js
+++ sites/terraludis.org/themes/tl_ruban/js/scripts.js
@@ -1,11 +1,23 @@
-// JavaScript Document
+/*
 (function($) {
 $(window).load(function(){
-	 $('input:-webkit-autofill').each(function(){
-		var text = $(this).val();
-		var name = $(this).attr('name');
-		$(this).after(this.outerHTML).remove();
-		$('input[name=' + name + ']').val(text);
-	});
+  $('input:-webkit-autofill').each(function(){
+    var text = $(this).val();
+    var name = $(this).attr('name');
+    $(this).after(this.outerHTML).remove();
+    $('input[name=' + name + ']').val(text);
+  });
+});
+})(jQuery);
+*/
+
+/**
+ * Cache les checkbox des listes de choix d'univers
+ * Ajoute une classe 'active' aux labels des checkboxes cochées 
+ */
+(function($) {
+$(window).load(function(){
+  $('.views-widget-filter-group input').hide()
+  .filter(':checked').next().addClass('active');
 });
 })(jQuery);
