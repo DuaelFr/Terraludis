65b859bed7a7a0aa21689105d7e3255a40f3caf5
Edouard Cunibil <admin@duael.fr>
2014-06-12 01:41:09 +0200

    Add drush aliases

---
+++ sites/all/drush/aliases.drushrc.php
@@ -0,0 +1,15 @@
+<?php
+
+$aliases['loc'] = array(
+  'uri' => 'local.terraludis.org',
+);
+$aliases['dev'] = $aliases['loc'];
+
+$aliases['test'] = array(
+  'uri' => 'test.terraludis.org',
+);
+
+$aliases['live'] = array(
+  'uri' => 'terraludis.org',
+);
+
